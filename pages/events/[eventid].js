efe5cbc7a05e58d97c4ddc835d285543c42607f2
Ankit Sharma <ankitsharma290101@gmail.com>
2024-02-13 17:12:09 +0530

    Dummy Data + Images + Routes

---
+++ pages/events/[eventid].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventDetail() {
+  return (
+    <div>
+      <h1>Events Detail</h1>
+    </div>
+  );
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Events() {
+export default function AllEventsPage() {
   return (
     <div>
-      <h1>Events</h1>
+      <h1>All Events</h1>
     </div>
   );
 }
