9f8dea6a7b737e57071f13f644523e6768d7d025
Ankit Sharma <ankitsharma290101@gmail.com>
2024-02-16 17:54:00 +0530

    added icons + button

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import React from "react";
 import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import AddressIcon from "../icons/address-icon";
+import DateIcon from "../icons/date-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props) {
   const { title, date, id, location, image } = props;
@@ -18,14 +22,22 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h1>{title}</h1>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanRedableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span> Explore Event</span>
+            <span className={classes.icon}>
+              {" "}
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,13 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  const tenant = router.query;
+  console.log(router.query);
   return (
     <div>
       <h1>FilteredEventsPage</h1>
-    </div>
+     </div>
   );
 }
