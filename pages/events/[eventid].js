44dc5f6f29d3e007cb68fac06b23d39398a180d9
Ankit Sharma <ankitsharma290101@gmail.com>
2024-02-18 12:29:51 +0530

    Event Page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,31 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
 
 export default function EventDetail() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event Found </p>;
+  }
+
   return (
-    <div>
-      <h1>Events Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
