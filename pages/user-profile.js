202bf4bf08324b9707eae0abdac49e07900a046b
Ankit Sharma <ankitsharma290101@gmail.com>
2024-02-20 18:48:11 +0530

    getServerSideProps

---
+++ pages/user-profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: { username: "Max" },
+  };
+}
