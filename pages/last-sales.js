e4ef7b24eb10ae28351e4fcf9f27b57c0913c901
Ankit Sharma <ankitsharma290101@gmail.com>
2024-02-20 22:46:02 +0530

    useSWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,34 +1,64 @@
 import React, { useEffect, useState } from "react";
-
+import useSWR from "swr";
 export default function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+  {
+    //   useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch("https://next-js-dd898-default-rtdb.firebaseio.com/Sales.json")
+    //       .then((response) => response.json())
+    //       .then((data) => {
+    //         const transformedSales = [];
+    //         for (const key in data) {
+    //           transformedSales.push({
+    //             id: key,
+    //             username: data[key].username,
+    //             volume: data[key].volume,
+    //           });
+    //         }
+    //         setSales(transformedSales);
+    //         setIsLoading(false);
+    //       });
+    //   }, []);
+  }
+  //   if (isLoading) {
+  //     return <p>Loading ...</p>;
+  //   }
+
+  //   if (!sales) {
+  //     return <p>No data yet</p>;
+  //   }
+
+  const { data, error } = useSWR(
+    "https://next-js-dd898-default-rtdb.firebaseio.com/Sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  //   const { data, error } = useSWR(
+  //     "https://next-js-dd898-default-rtdb.firebaseio.com/Sales.json"
+  //   );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://next-js-dd898-default-rtdb.firebaseio.com/Sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading ...</p>;
+  if (error) {
+    return <p>Failed to load data </p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (!data || !sales) {
+    return <p>Loading ...</p>;
   }
 
   return (
