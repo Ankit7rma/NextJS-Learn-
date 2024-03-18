fc0d9d9b8a12ee24fa5719509659caae451dc9db
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-18 21:45:25 +0530

    Created Backend For POST req and Save Data in File

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ function HomePage() {
 
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
+
+    fetch(); //{email:"ank@g,ail.com",text:"This is Post Req"}
   }
   return (
     <div>
