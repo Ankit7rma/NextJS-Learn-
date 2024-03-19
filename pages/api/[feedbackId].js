373836bf46b7809e850bc0820724e41c515cd7b5
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-19 17:13:04 +0530

    Dynamic API Routes

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,12 @@
+import { buildFeedPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  const feedbackId = req.query.feedbackId;
+  const filePath = buildFeedPath();
+  const feedbackData = extractFeedback(filePath);
+  const selectedFeedback = feedbackData.find(
+    (feedback) => feedback.id === feedbackId
+  );
+  res.status(200).json({ feedback: selectedFeedback });
+}
+export default handler;
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,14 +1,29 @@
+import { Fragment, useState } from "react";
 import { buildFeedPath, extractFeedback } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>
-          <strong>Email:</strong> {item.email} - {item.feedback}
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.feedback}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details{" "}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
